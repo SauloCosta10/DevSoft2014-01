0a794bc65bc203bf3c9a8cc1f9aa8e4c7dd40d16
Saulo de Tarso <saulo.costa@usp.br>
2014-03-25 15:49:16 -0300

    Ex 1 Aula 1

---
+++ aula1/ex1.1.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+mkdir devsoft
+mkdir praticas_desenvolvimento_software
+cd praticas_desenvolvimento_software
+ln -s praticas_desenvolvimento_software devsoft
+mkdir Saulo_de_Tarso
+mkdir Saulo
+ln -s Saulo_de_Tarso Saulo
+mkdir NUSP
+ln -s Saulo_de_Tarso NUSP
+cd Saulo_de_Tarso
+mkdir aula01
+cd aula01
+nano ex1.1.sh
